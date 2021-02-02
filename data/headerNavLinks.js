3aaacd57545bf5c4447846d154ad9dc5e832e4c7
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-02 17:30:02 -0500

    Added logo & nav links
    Logo references: Figma (Source Serif Pro, green #206a5d from https://colorhunt.co/palette/201883 )
    
    Scale SVG https://css-tricks.com/scale-svg/
    
    Compress SVG https://vecta.io/nano

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,10 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
+  { href: '/service-offerings', title: 'Service Offerings' },
+  { href: '/sample-publications', title: 'Sample Publications' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/contact', title: 'Contact' },
 ]
 
 export default headerNavLinks
