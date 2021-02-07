8bd9dfff42c0307595c1da206a3d4612dcb9b296
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-07 16:06:12 -0500

    Add approved images

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Link from '@/components/Link'
 import { PageSeo } from '@/components/SEO'
 import ImageSlideshow from '@/components/KeenSlider'
 import SectionContainer from '@/components/SectionContainer'
+import Image from 'next/image'
 
 const MAX_DISPLAY = 5
 const postDateTemplate = tinytime('{MMMM} {DD}, {YYYY}')
@@ -29,22 +30,37 @@ export default function Home({ posts }) {
           <ImageSlideshow />
         </SectionContainer>
       </div>
-      <div className="grid text-center bg-gray-300 grid-col-1 md:grid-cols-3">
-        <div>Service Offerings</div>
-        <div>Sample Publications</div>
-        <div>Blog</div>
+      <div className="bg-gray-300">
+        <SectionContainer>
+          <div className="grid text-center grid-col-1 md:grid-cols-3">
+            <div>
+              <div className="w-3/4 rounded-3xl">
+                <Image
+                  alt="ocean"
+                  src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
+                  width={2177}
+                  height={3265}
+                />
+              </div>
+              <h3 className="font-serif">Service Offerings</h3>
+            </div>
+            <div>Sample Publications</div>
+            <div>Blog</div>
+          </div>
+        </SectionContainer>
       </div>
       <div className="bg-green-800">
-        About Skip to content Steven Terner Steven TernerConsultant specializing in the Middle East
-        Home About Service Offerings Sample Publications Blog Contact Service Offerings I offer
-        expert consulting services for international businesses, newspapers, NGOs & think tanks.
-        Sample Publications My consulting portfolio includes examples of my analysis of the Middle
-        East & Central/West Asia. Blog I publish my latest socio-political analysis work here on my
-        blog at StevenTerner.com About Steven Terner is a multi-lingual American scholar and
-        researcher specializing in socio-political and economic analysis of the Middle East and
-        Central/West Asia. Read more Steven Terner contact@StevenTerner.com Get in touch! EditHome
-        About Service Offerings Sample Publications Blog Contact Steven Terner, Create your website
-        at WordPress.com Get started :)
+        <SectionContainer>
+          About Skip to content Steven Terner Steven TernerConsultant specializing in the Middle
+          East Home About Service Offerings Sample Publications Blog Contact Service Offerings I
+          offer expert consulting services for international businesses, newspapers, NGOs & think
+          tanks. Sample Publications My consulting portfolio includes examples of my analysis of the
+          Middle East & Central/West Asia. Blog I publish my latest socio-political analysis work
+          here on my blog at StevenTerner.com About Steven Terner is a multi-lingual American
+          scholar and researcher specializing in socio-political and economic analysis of the Middle
+          East and Central/West Asia. Read more Steven Terner contact@StevenTerner.com Get in touch!
+          EditHome About Service Offerings Sample Publications Blog Contact Steven Terner
+        </SectionContainer>
       </div>
     </>
   )
