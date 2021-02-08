9fd389435a7aad231918d7ffd7cfc7b718844c8f
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-07 19:30:26 -0500

    Fix line endings

--- pages/index.js
+++ pages/index.js
@@ -32,50 +32,48 @@ export default function Home({ posts }) {
       </div>
       <div className="bg-gray-300">
         <SectionContainer>
-          <div className="max-h-screen h-3/4">
-            <div className="grid grid-cols-3 gap-1 text-center md:gap-2 xl:gap-3">
-              <div>
-                <Image
-                  alt="ocean"
-                  src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
-                  width={2177}
-                  height={3265}
-                  className="object-cover"
-                />
-                <h3 className="fontSerif">Services</h3>
-                <p>
-                  I offer expert consulting services for international businesses, newspapers, NGOs
-                  & think tanks.
-                </p>
-              </div>
-              <div>
-                <Image
-                  alt="ocean"
-                  src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
-                  width={2177}
-                  height={3265}
-                  className="object-cover"
-                />
-                <h3 className="fontSerif">Portfolio</h3>
-                <p>
-                  My consulting portfolio includes examples of my analysis of the Middle East &
-                  Central/West Asia.
-                </p>
-              </div>
-              <div>
-                <Image
-                  alt="ocean"
-                  src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
-                  width={2177}
-                  height={3265}
-                  className="object-cover"
-                />
-                <h3 className="fontSerif">Blog</h3>
-                <p>
-                  I publish my latest socio-political analysis work here on my blog at
-                  StevenTerner.com
-                </p>
-              </div>
+          <div className="grid grid-cols-3 gap-1 text-center keen-slider__slide md:gap-2 xl:gap-3">
+            <div>
+              <Image
+                alt="ocean"
+                src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
+                width={2177}
+                height={3265}
+                className="object-cover rounded-full"
+              />
+              <h3 className="fontSerif">Services</h3>
+              <p>
+                I offer expert consulting services for international businesses, newspapers, NGOs &
+                think tanks.
+              </p>
+            </div>
+            <div>
+              <Image
+                alt="ocean"
+                src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
+                width={2177}
+                height={3265}
+                className="object-cover"
+              />
+              <h3 className="fontSerif">Portfolio</h3>
+              <p>
+                My consulting portfolio includes examples of my analysis of the Middle East &
+                Central/West Asia.
+              </p>
+            </div>
+            <div>
+              <Image
+                alt="ocean"
+                src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
+                width={2177}
+                height={3265}
+                className="object-cover"
+              />
+              <h3 className="fontSerif">Blog</h3>
+              <p>
+                I publish my latest socio-political analysis work here on my blog at
+                StevenTerner.com
+              </p>
             </div>
           </div>
         </SectionContainer>
