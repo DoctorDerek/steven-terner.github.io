5d8a0f02748ed2aeccce6b1586f831b62571602c
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-07 21:20:22 -0500

    Added more responsive image sizes smaller than 640

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+  images: {
+    imageSizes: [], // added to deviceSizes
+    deviceSizes: [
+      16,
+      32,
+      48,
+      64,
+      96,
+      128,
+      256,
+      384,
+      512,
+      640,
+      750,
+      828,
+      1080,
+      1200,
+      1920,
+      2048,
+      3840,
+    ],
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   experimental: {
     modern: true,
