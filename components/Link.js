c9e34cd8b1fd57431340cf9b22197d8e174e2b71
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-12 18:48:02 -0500

    Add default styles to Link

--- components/Link.js
+++ components/Link.js
@@ -1,23 +1,26 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from 'next/link'
 
-const CustomLink = ({ href, ...rest }) => {
+const CustomLink = ({ href, className, ...rest }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
+
+  const defaultClass = 'underline text-green-dark hover:text-green hover:no-underline'
+  className = className ? className + defaultClass : defaultClass
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <a {...rest} />
+        <a {...rest} className={className} />
       </Link>
     )
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />
+    return <a href={href} {...rest} className={className} />
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} className={className} />
 }
 
 export default CustomLink
