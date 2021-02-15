7d542cf262b1ea92651674c90d30d31d493c9599
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-14 20:51:45 -0500

    Fix space-between on Cards (only have space-between on mobile 1-column display)

--- components/CardList.js
+++ components/CardList.js
@@ -1,6 +1,6 @@
 const CardList = ({ children }) => {
   return (
-    <div className="grid grid-cols-1 gap-1 space-y-4 md:grid-cols-3 md:gap-2 xl:gap-3">
+    <div className="grid grid-cols-1 gap-1 space-y-4 md:space-y-0 md:grid-cols-3 md:gap-2 xl:gap-3">
       {children}
     </div>
   )
