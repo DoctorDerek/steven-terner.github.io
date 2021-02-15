322e0d71b89cd7c730b8ad0a172bc6aa13410a4e
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-14 20:00:18 -0500

    Add vertical space between Cards (on mobile)

--- components/CardList.js
+++ components/CardList.js
@@ -1,5 +1,9 @@
 const CardList = ({ children }) => {
-  return <div className="grid grid-cols-1 gap-1 md:grid-cols-3 md:gap-2 xl:gap-3">{children}</div>
+  return (
+    <div className="grid grid-cols-1 gap-1 space-y-4 md:grid-cols-3 md:gap-2 xl:gap-3">
+      {children}
+    </div>
+  )
 }
 
 export default CardList
