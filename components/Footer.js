48ddcd19c3067abd9c9165f038df5ca39be0cfa7
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-23 17:43:06 -0500

    Align menu items properly in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,30 @@
 import Link from "./Link"
 import siteMetadata from "@/data/siteMetadata"
-import FooterNav from "./FooterNav"
 import SectionContainer from "./SectionContainer"
+import headerNavLinks from "@/data/headerNavLinks"
 
 export default function Footer() {
   return (
-    <SectionContainer>
-      <footer className="flex flex-row w-full mt-16 xl:text-lg md:text-md sm:flex sm:text-sm">
-        <div className="flex flex-grow mb-2 space-x-2 text-gray-900 dark:text-gray-400">
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <Link href="/">{siteMetadata.author}</Link>
-        </div>
-        <div className="mb-8 text-sm">
-          <FooterNav />
-        </div>
-      </footer>
-    </SectionContainer>
+    <div className="w-full">
+      <SectionContainer>
+        <footer className="flex flex-col justify-start w-full py-1 mt-8 mb-4 font-serif text-base font-bold leading-5 text-center border-gray-900 rounded sm:flex-row dark:border-gray-500">
+          <div className="flex flex-grow px-3 py-1 space-x-2 text-gray-900 dark:text-gray-400">
+            <div>{`© ${new Date().getFullYear()}`}</div>
+            <Link href="/">{siteMetadata.author}</Link>
+          </div>
+          <div className="flex flex-col justify-start sm:flex-row">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="px-3 py-1 text-black no-underline rounded hover:bg-green-light dark:hover:text-green-dark"
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </footer>
+      </SectionContainer>
+    </div>
   )
 }
