b70675975009e7d563b0c0c3c52b064d584e42bb
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-23 18:59:13 -0500

    Left-align footer by removing left padding

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
     <div className="w-full">
       <SectionContainer>
         <footer className="flex flex-col justify-start w-full py-1 mt-8 mb-4 font-serif text-base font-bold leading-5 text-center border-gray-900 rounded sm:flex-row dark:border-gray-500">
-          <div className="flex flex-grow px-3 py-1 space-x-2 text-gray-900 dark:text-gray-400">
+          <div className="flex flex-grow py-1 space-x-2 text-gray-900 dark:text-gray-400">
             <div>{`© ${new Date().getFullYear()}`}</div>
             <Link href="/">{siteMetadata.author}</Link>
           </div>
