b3ead64dd46123fb48172308f74e7dccae4405b0
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-25 21:03:37 -0500

    Refactor SectionContainer to accept a fullWidth prop for pages like /about

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,7 +1,15 @@
-export default function SectionContainer({ children }) {
+export default function SectionContainer({ children, fullWidth = false }) {
+  // fullWidth === true implies no max-width
   return (
-    <div className="w-full max-w-sm px-4 mx-auto sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
-      {children}
-    </div>
+    <>
+      {!fullWidth && (
+        <div className="w-full max-w-sm px-4 mx-auto sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
+          {children}
+        </div>
+      )}
+      {fullWidth && (
+        <div className="w-full px-4 mx-auto sm:px-6">{children}</div>
+      )}
+    </>
   )
 }
