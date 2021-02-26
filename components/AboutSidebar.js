705909fdfa4ab59a08cff74172c659a05e9336b9
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-02-25 19:10:05 -0500

    Add photo to AboutSidebar

--- components/AboutSidebar.js
+++ components/AboutSidebar.js
@@ -3,8 +3,20 @@ import Link from "@/components/Link"
 import SocialIcon from "@/components/social-icons"
 import Image from "next/image"
 
-const AboutSidebar = () => (
+const AboutSidebar = ({ includePhoto = false }) => (
   <div className="flex flex-col items-center col-span-1 py-12 space-y-12 border-2 border-gray-800 border-solid sm:py-0 sm:space-y-0 rounded-3xl sm:col-span-3 sm:border-0">
+    {includePhoto && (
+      <div className="relative w-60 h-60">
+        <Image
+          alt="ocean"
+          src="/static/images/StevenTernerPhoto2-cropped.jpg"
+          layout="fill"
+          className="object-cover rounded-full"
+          sizes="33vw"
+          quality="75"
+        />
+      </div>
+    )}
     <h2 className="font-serif text-4xl font-bold sm:py-4 md:text-4xl">
       Steven Terner
     </h2>
