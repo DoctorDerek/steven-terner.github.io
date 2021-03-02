74a1ab1c855d628059cdc7bc8b7e81cf1762a729
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-03-01 20:04:26 -0500

    Add content-visibility: auto and contain-intrinsic-size to SectionContainers
    See further: https://css-tricks.com/more-on-content-visibility/

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -3,12 +3,12 @@ export default function SectionContainer({ children, fullWidth = false }) {
   return (
     <>
       {!fullWidth && (
-        <div className="w-full max-w-sm px-4 mx-auto sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
+        <section className="w-full max-w-sm px-4 mx-auto sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
           {children}
-        </div>
+        </section>
       )}
       {fullWidth && (
-        <div className="w-full px-4 mx-auto sm:px-6">{children}</div>
+        <section className="w-full px-4 mx-auto sm:px-6">{children}</section>
       )}
     </>
   )
