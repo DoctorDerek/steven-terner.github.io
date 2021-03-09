75676971f0b7bfc0b5f0db26978d07c72fc166f3
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 22:32:43 -0500

    Fix mobile navigation menu bug; it wasn't appearing because of content-visibility: auto

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -9,33 +9,31 @@ import ThemeSwitch from "./ThemeSwitch"
 
 const LayoutWrapper = ({ children }) => {
   return (
-    <div className="box-border flex flex-col justify-between w-full h-screen max-w-full">
-      <header className="flex flex-col items-center justify-between w-full pt-10">
-        <SectionContainer>
-          <Link href="/" aria-label="Steven Terner" className={null}>
-            <Logo />
-          </Link>
-          <div className="flex flex-row-reverse items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-row">
-            <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-500 xl:text-2xl md:text-xl sm:flex sm:text-l">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="px-3 py-1 no-underline rounded-3xl xl:px-8 text-green-dark hover:bg-green-light hover:text-gray-900"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-            <ThemeSwitch />
-            <MobileNav />
+    <>
+      <SectionContainer header={true} className="pt-10">
+        <Link href="/" aria-label="Steven Terner" className={null}>
+          <Logo />
+        </Link>
+        <div className="flex flex-row-reverse items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-row">
+          <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-500 xl:text-2xl md:text-xl sm:flex sm:text-l">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="px-3 py-1 no-underline rounded-3xl xl:px-8 text-green-dark hover:bg-green-light hover:text-gray-900"
+              >
+                {link.title}
+              </Link>
+            ))}
           </div>
-        </SectionContainer>
-      </header>
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </SectionContainer>
 
-      <main className="mb-auto">{children}</main>
+      <main>{children}</main>
       <Footer />
-    </div>
+    </>
   )
 }
 
--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,15 +1,27 @@
-export default function SectionContainer({ children, fullWidth = false }) {
+export default function SectionContainer({
+  children,
+  fullWidth = false,
+  header = false,
+  className,
+}) {
   // fullWidth === true implies no max-width
-  return (
-    <>
-      {!fullWidth && (
-        <section className="w-full max-w-sm px-4 mx-auto sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
-          {children}
-        </section>
-      )}
-      {fullWidth && (
-        <section className="w-full px-4 mx-auto sm:px-6">{children}</section>
-      )}
-    </>
+  // header returns a <header> instead of a <section>
+  // className, if present, is appended to the defaults
+
+  const defaultClass = fullWidth
+    ? "w-full px-4 mx-auto sm:px-6"
+    : "w-full max-w-sm px-4 mx-auto sm:max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0"
+
+  if (className) {
+    className = defaultClass + " " + className
+  } else if (className !== null) {
+    // null represents intentional absence of any className
+    className = defaultClass
+  }
+
+  return header ? (
+    <header className={className}>{children}</header>
+  ) : (
+    <section className={className}>{children}</section>
   )
 }
