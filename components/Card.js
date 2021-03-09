355451c59b575a66c05adc732bfc3905499d1d19
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 20:20:54 -0500

    Revert "Add border at top & bottom of CardList on mobile display"
    This reverts commit 56bcdf0949a9b3c3089ca6064a428077c40be38d.

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import Link from "@/components/Link"
 
 const Card = ({ alt, src, link, heading, children }) => {
   return (
-    <div className="py-8 md:px-3 sm:px-4 sm:first:pl-0 sm:first:pr-4 sm:last:pl-4 sm:last:pr-0 md:first:pr-3 md:last:pl-3 sm:py-0">
+    <div className="py-8 md:px-3 sm:px-4 sm:first:pl-0 sm:first:pr-4 sm:last:pl-4 sm:last:pr-0 md:first:pr-3 md:last:pl-3 sm:py-0 first:pt-0 first:pb-8 last:pt-8 last:pb-0 sm:first:py-0 sm:last:py-0">
       {src && (
         <div className="relative max-w-xs m-auto h-36">
           <Image
