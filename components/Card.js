698157d1afcb01ee12e4d4340342ab65c2645146
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 20:52:33 -0500

    Fix column-sizing bug caused by extraneous max-w-xs

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ alt, src, link, heading, children }) => {
   return (
     <div className="py-8 md:px-3 sm:px-4 sm:first:pl-0 sm:first:pr-4 sm:last:pl-4 sm:last:pr-0 md:first:pr-3 md:last:pl-3 sm:py-0 first:pt-0 first:pb-8 last:pt-8 last:pb-0 sm:first:py-0 sm:last:py-0">
       {src && (
-        <div className="relative max-w-xs m-auto h-36">
+        <div className="relative m-auto h-36">
           <Image
             alt={alt}
             src={src}
