11a56e3b724cb43a9b06ee4385bc15516b0db2d1
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 21:19:58 -0500

    Make nav menu more rounded (rounded -> rounded-3xl)

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -20,12 +20,12 @@ const LayoutWrapper = ({ children }) => {
         </Link>
         <SectionContainer>
           <div className="flex flex-row-reverse items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-row">
-            <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded dark:border-gray-500 xl:text-2xl md:text-xl sm:flex sm:text-l">
+            <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-500 xl:text-2xl md:text-xl sm:flex sm:text-l">
               {headerNavLinks.map((link) => (
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="px-3 py-1 no-underline rounded xl:px-8 text-green-dark hover:bg-green-light hover:text-gray-900"
+                  className="px-3 py-1 no-underline rounded-3xl xl:px-8 text-green-dark hover:bg-green-light hover:text-gray-900"
                 >
                   {link.title}
                 </Link>
