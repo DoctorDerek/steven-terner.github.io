02f0aa43c7a762080d7a8a7d94311f5692a639e7
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 20:18:47 -0500

    Add border between Cards in CardList

--- components/Card.js
+++ components/Card.js
@@ -3,9 +3,9 @@ import Link from "@/components/Link"
 
 const Card = ({ alt, src, link, heading, children }) => {
   return (
-    <div>
+    <div className="py-8 md:px-3 sm:px-4 sm:first:pl-0 sm:first:pr-4 sm:last:pl-4 sm:last:pr-0 md:first:pr-3 md:last:pl-3 sm:py-0 first:pt-0 first:pb-8 last:pt-8 last:pb-0 sm:first:py-0 sm:last:py-0">
       {src && (
-        <div className="relative max-w-xs m-auto h-36 ">
+        <div className="relative max-w-xs m-auto h-36">
           <Image
             alt={alt}
             src={src}
--- components/CardList.js
+++ components/CardList.js
@@ -1,6 +1,6 @@
 const CardList = ({ children }) => {
   return (
-    <div className="grid grid-cols-1 gap-1 space-y-12 sm:space-y-0 sm:grid-cols-3 sm:gap-2 xl:gap-3">
+    <div className="grid w-full grid-cols-1 border-gray-700 border-solid divide-y-2 divide-gray-600 sm:divide-x-2 xl:divide-x-4 sm:divide-y-0 divide-solid sm:grid-cols-3 sm:border-0">
       {children}
     </div>
   )
