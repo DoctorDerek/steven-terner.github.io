0de2cff31056ed7ec18f998b84254a23fadb9221
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 21:32:34 -0500

    Remove extra SectionContainer

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -15,8 +15,6 @@ const LayoutWrapper = ({ children }) => {
           <Link href="/" aria-label="Steven Terner" className={null}>
             <Logo />
           </Link>
-        </SectionContainer>
-        <SectionContainer>
           <div className="flex flex-row-reverse items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-row">
             <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-500 xl:text-2xl md:text-xl sm:flex sm:text-l">
               {headerNavLinks.map((link) => (
