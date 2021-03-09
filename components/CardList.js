56bcdf0949a9b3c3089ca6064a428077c40be38d
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-08 20:20:48 -0500

    Add border at top & bottom of CardList on mobile display

--- components/CardList.js
+++ components/CardList.js
@@ -1,6 +1,6 @@
 const CardList = ({ children }) => {
   return (
-    <div className="grid w-full grid-cols-1 border-gray-700 border-solid divide-y-2 divide-gray-600 sm:divide-x-2 xl:divide-x-4 sm:divide-y-0 divide-solid sm:grid-cols-3 sm:border-0">
+    <div className="grid w-full grid-cols-1 border-t-2 border-b-2 border-gray-700 border-solid divide-y-2 divide-gray-600 sm:divide-x-2 xl:divide-x-4 sm:divide-y-0 divide-solid sm:grid-cols-3 sm:border-0">
       {children}
     </div>
   )
