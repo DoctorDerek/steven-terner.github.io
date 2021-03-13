f4aee22fbcf8c8f2955e5b1f8591752b86a83fa6
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-12 20:43:19 -0500

    Refactor Slideshow to SlideshowSlides

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -1,11 +1,10 @@
-import React from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { useKeenSlider } from "keen-slider/react"
-import Image from "@/components/Image"
-import SlideshowSlide from "@/components/SlideshowSlide"
+import SlideshowSlides from "@/components/SlideshowSlides"
 
 const Slideshow = () => {
-  const [pause, setPause] = React.useState(false)
-  const timer = React.useRef()
+  const [pause, setPause] = useState(false)
+  const timer = useRef()
   const [sliderRef, slider] = useKeenSlider({
     loop: true,
     duration: 3000, // animation duration in ms
@@ -17,7 +16,7 @@ const Slideshow = () => {
     },
   })
 
-  React.useEffect(() => {
+  useEffect(() => {
     sliderRef.current.addEventListener("mouseover", () => {
       setPause(true)
     })
@@ -26,7 +25,7 @@ const Slideshow = () => {
     })
   }, [sliderRef])
 
-  React.useEffect(() => {
+  useEffect(() => {
     timer.current = setInterval(() => {
       if (!pause && slider) {
         slider.next()
@@ -42,57 +41,7 @@ const Slideshow = () => {
       ref={sliderRef}
       className="h-60 sm:h-80 md:h-112 lg:h-112 xl:h-128 keen-slider"
     >
-      <SlideshowSlide>
-        <Image
-          alt="ocean"
-          src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
-          sizes="portrait"
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/behzad-ghaffarian-agGyKOiowuk-unsplash.jpg"
-          sizes="portrait"
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/mehrshad-rajabi-UI8kIVUu7oI-unsplash.jpg"
-          sizes="portrait"
-        />
-      </SlideshowSlide>
-      <SlideshowSlide>
-        <Image
-          alt="ocean"
-          src="/static/images/milad-b-fakurian-q539g9f0cYw-unsplash.jpg"
-          sizes="landscape"
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/mostafa-meraji-wH_5YAylHnc-unsplash.jpg"
-          sizes="landscape"
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/pourya-daneshvar-cYmRHwySr3U-unsplash.jpg"
-          sizes="landscape"
-        />
-      </SlideshowSlide>
-      <SlideshowSlide>
-        <Image
-          alt="ocean"
-          src="/static/images/roozbeh-eslami-o2E9zgoRFgc-unsplash.jpg"
-          sizes="landscape"
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/sajad-nori-cacpYWjYEt0-unsplash.jpg"
-          sizes="landscape"
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/Imports, Exports, and International Partnerships photo.jpg"
-          sizes="landscape"
-        />
-      </SlideshowSlide>
+      <SlideshowSlides />
     </div>
   )
 }
