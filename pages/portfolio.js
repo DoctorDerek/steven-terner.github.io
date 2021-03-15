89522e76b18e321d37db3da57eda8f1eceb392ce
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-15 14:41:34 -0500

    Add text to portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,12 +3,11 @@ import AboutSidebar from "@/components/AboutSidebar"
 import { PageSeo } from "@/components/SEO"
 import SectionContainer from "@/components/SectionContainer"
 import PageTitle from "@/components/PageTitle"
-import Twemoji from "@/components/Twemoji"
 
 export default function Portfolio() {
   return (
     <>
-      <PageSeoqb
+      <PageSeo
         title={`Portfolio - ${siteMetadata.author}`}
         description={`Portfolio - ${siteMetadata.author}`}
         url={`${siteMetadata.siteUrl}/Portfolio`}
@@ -17,8 +16,7 @@ export default function Portfolio() {
       <div className="bg-gradient-to-b from-blue-light to-blue-dark">
         <SectionContainer fullWidth={true}>
           <h2 className="px-16 py-16 font-serif text-5xl font-bold max-w-7xl leading-14">
-            Multi-linguistic international business consulting for the Middle
-            East and Central/West Asia.
+            Sample Publications
           </h2>
         </SectionContainer>
       </div>
@@ -26,64 +24,33 @@ export default function Portfolio() {
         <div className="grid grid-cols-1 px-16 py-32 text-xl sm:gap-16 sm:grid-cols-3">
           <div>
             <p className="text-4xl">
-              “I can help your business access new markets with
-              culturally-sensitive and effective business strategy.”
+              My portfolio showcases my writing analyzing complex geopolitical
+              situations and international business opportunities in the Middle
+              East and Central/West Asia.
             </p>
-            <p className="pt-8 text-right">—Steven Terner</p>
+            <p className="pt-8 text-right">
+              I’m available for immediate hire for consulting services. Please
+              get in touch.
+            </p>
           </div>
-          <ul>
-            <li>
-              <Twemoji emoji="🤝" />
-              Identify international business partners
-            </li>
-            <li>
-              <Twemoji emoji="📝" />
-              Negotiate contracts
-            </li>
-            <li>
-              <Twemoji emoji="📈" />
-              Design and oversee product strategy
-            </li>
-            <li>
-              <Twemoji emoji="📊" />
-              Foreign market analysis
-            </li>
-            <li>
-              <Twemoji emoji="🕵️‍♂️" />
-              Due diligence support
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <Twemoji emoji="💱" />
-              Assessment of economic opportunities
-            </li>
-            <li>
-              <Twemoji emoji="📰" />
-              Foreign media monitoring
-            </li>
-            <li>
-              <Twemoji emoji="📕" />
-              Training and lectures
-            </li>
-            <li>
-              <Twemoji emoji="✍" />
-              Written geopolitical analysis
-            </li>
-            <li>
-              <Twemoji emoji="💭" />
-              Appraisal of geopolitical risks
-            </li>
-          </ul>
         </div>
       </SectionContainer>
       <div className="text-white bg-black">
         <SectionContainer fullWidth={true}>
-          <div className="grid grid-cols-8 px-16 py-32">
+          <div className="grid grid-cols-2 px-16 py-32">
             <p className="col-span-5 py-16 text-3xl leading-relaxed">
-              My consulting experience includes the <em>Jerusalem Post</em>, the
-              Indian Embassy in Israel, and the Atlantic Council and Hudson
-              Institute think tanks in Washington, DC.
+              Publicly Available Work A selection of my written analysis about
+              the Middle East and Central/West Asia. “How the US Can Win Back
+              Clout in Syria” in Fair Observer, Oct 06, 2020 “Iran’s muddled
+              relationship with cryptocurrency is self-inflicted” in Atlantic
+              Council, Jun 18, 2020 Iranian Economic News Analysis of current
+              trends in the economy of Iran, including COVID-19. “Corona is the
+              latest battleground between Rouhani and the IRGC” (PDF) in Iranian
+              Economic News, May 1, 2020 “Economists Make Policy Recommendations
+              in Open Letter to President” (PDF) in Iranian Economic News, Jan
+              20, 2020 Let me help you act on business opportunities in the
+              Middle East and Central/West Asia. Steven Terner
+              contact@StevenTerner.com Get in touch!
             </p>
             <AboutSidebar />
           </div>
