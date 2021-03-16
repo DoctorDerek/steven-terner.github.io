cd4f39660e5db8928c9cd6a5b847baf18ee86b85
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-16 00:51:30 -0500

    Add dark mode to main nav menu

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -22,7 +22,7 @@ const LayoutWrapper = ({ children }) => {
               <Link
                 key={link.title}
                 href={link.href}
-                className="px-3 py-1 no-underline rounded-3xl xl:px-8 text-green-dark hover:bg-green-light hover:text-gray-900"
+                className="px-3 py-1 no-underline rounded-3xl xl:px-8 text-green-dark hover:bg-green-light hover:text-gray-900 dark:text-green-light dark:hover:text-green-lightest dark:hover:bg-green-dark"
                 //style={{ webkitTextStroke: "1px rgb(2,132,199)" }} // text-blue-600
               >
                 {link.title}
