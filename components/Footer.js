170d5e5a136f44909e28cfbc6c243cf0b66e4b3f
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-20 17:43:43 -0500

    Removed unused border classes

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import headerNavLinks from "@/data/headerNavLinks"
 export default function Footer() {
   return (
     <SectionContainer>
-      <footer className="flex flex-wrap w-full py-1 mt-8 mb-4 font-serif font-bold leading-5 text-center border-gray-900 rounded md:text-lg xl:text-xl dark:border-gray-500">
+      <footer className="flex flex-wrap w-full py-1 mt-8 mb-4 font-serif font-bold leading-5 text-center rounded md:text-lg xl:text-xl">
         <div className="flex flex-grow py-1 space-x-2 text-gray-900 dark:text-gray-400">
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{siteMetadata.author}</div>
