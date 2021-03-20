eff31c50301253b7f0accf199b54cb643a7a80df
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-20 18:05:58 -0500

    Change dark mode Footer name & copyright color from text-gray-100 -> text-green-dark

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <SectionContainer>
       <footer className="flex flex-wrap w-full py-1 mt-8 mb-4 font-serif font-bold leading-5 text-center rounded md:text-lg xl:text-xl">
-        <div className="flex flex-grow py-1 space-x-2 text-gray-900 dark:text-gray-100">
+        <div className="flex flex-grow py-1 space-x-2 text-gray-900 dark:text-green-dark">
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{siteMetadata.author}</div>
         </div>
