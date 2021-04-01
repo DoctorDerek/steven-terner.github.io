77fc799e0f42733e99e175efc11963538404fd02
Dr. Derek Austin <derekraustin@gmail.com>
2021-03-31 19:58:01 -0500

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,20 +1,11 @@
 module.exports = {
-  editorconfig: true, // Read .editorconfig
-  // These settings are handled in .editorconfig:
-  // tabWidth: 2, // indent_size = 2
-  // useTabs: false, // indent_style = space
-  // endOfLine: 'lf', // end_of_line = lf
+  // These settings are duplicated in .editorconfig:
+  tabWidth: 2, // indent_size = 2
+  useTabs: false, // indent_style = space
+  endOfLine: "lf", // end_of_line = lf
   semi: false, // default: true
   singleQuote: false,
   printWidth: 80,
   trailingComma: "es5",
   bracketSpacing: true,
-  overrides: [
-    {
-      files: "*.js",
-      options: {
-        parser: "flow",
-      },
-    },
-  ],
 }
