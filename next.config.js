94f8bdd4930aaebd8c630876438f874225264b35
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-15 21:19:43 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -71,13 +71,6 @@ module.exports = withBundleAnalyzer({
       })
     }
 
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      }
-    }
-
     return config
   },
 })
