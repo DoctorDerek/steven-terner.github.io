f41a3729580b8dd86f882ea471536ca16db61379
Dr. Derek Austin <derekraustin@gmail.com>
2021-04-30 13:49:55 -0500

    Increase font size on md:text-3xl to text-4xl

--- components/AboutSidebar.js
+++ components/AboutSidebar.js
@@ -5,7 +5,7 @@ import Image from "@/components/Image"
 
 const AboutSidebarInner = () => (
   <>
-    <h2 className="font-serif text-4xl font-bold text-center sm:text-3xl sm:py-4 md:text-3xl">
+    <h2 className="font-serif text-4xl font-bold text-center sm:text-3xl sm:py-4 md:text-4xl">
       Steven Terner, LLC
     </h2>
     <div className="flex flex-wrap w-full justify-evenly sm:py-12">
