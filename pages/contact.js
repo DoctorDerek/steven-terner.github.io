253d5bd2d897cc1177b6ef8310205c3d79245487
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-04 17:07:30 -0500

    Refactor to ContactForm component

--- pages/contact.js
+++ pages/contact.js
@@ -4,52 +4,7 @@ import { PageSeo } from "@/components/SEO"
 import SectionContainer from "@/components/SectionContainer"
 import PageTitle from "@/components/PageTitle"
 import Image from "@/components/Image"
-import { useForm, ValidationError } from "@formspree/react"
-
-function ContactForm() {
-  const [state, handleSubmit] = useForm("xrgreylk")
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        <strong>Name</strong>{" "}
-        <span className="text-xl md:text-2xl">(required)</span>
-      </label>
-      <input
-        type="text"
-        className="w-full h-10 pl-1 align-top border-2 border-black border-solid rounded-xl"
-        name="email"
-        id="email"
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <label htmlFor="email">
-        <strong>Email</strong>{" "}
-        <span className="text-xl md:text-2xl">(required)</span>
-      </label>
-      <input
-        type="text"
-        className="w-full h-10 pl-1 align-top border-2 border-black border-solid rounded-xl"
-        name="name"
-        id="name"
-      />
-      <label htmlFor="message">
-        <strong>Name</strong>{" "}
-        <span className="text-xl md:text-2xl">(required)</span>
-      </label>
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <input
-        type="textarea"
-        className="w-full pb-48 pl-1 align-top border-2 border-black border-solid rounded-xl h-60"
-        placeholder="Let me know how I can help"
-        name="message"
-        id="message"
-      />
-      {/*padding-bottom for vertical alignment of placeholder to top*/}
-    </form>
-  )
-}
+import ContactForm from "@/components/ContactForm"
 
 export default function Contact() {
   return (
