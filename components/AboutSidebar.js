f7ed41ebb9194133ab122d46ba6ff39a51a430a1
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-04 21:47:53 -0500

    Show border on hover

--- components/AboutSidebar.js
+++ components/AboutSidebar.js
@@ -26,7 +26,7 @@ const AboutSidebarHomePage = () => {
           }
           className="text-gray-100"
         >
-          <div className="px-4 py-2.5 mx-auto font-bold text-center rounded text-base bg-green-dark hover:bg-green-light hover:text-gray-900">
+          <div className="px-4 py-2.5 mx-auto font-bold text-center rounded text-base bg-green-dark border-transparent border-2 border-solid hover:border-gray-100">
             Get in touch
           </div>
         </Link>
