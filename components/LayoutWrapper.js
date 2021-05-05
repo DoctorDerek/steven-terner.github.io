c9a7a46b1b5b07b4731c64f5776702acd1a4b286
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-04 21:40:24 -0500

    Fix header wrap issue

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -35,7 +35,7 @@ export default LayoutWrapper
 function Header() {
   return (
     <div className="flex flex-row-reverse items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-row">
-      <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-100 xl:text-2xl md:text-xl sm:flex sm:text-l dark:bg-green-dark">
+      <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-100 xl:text-2xl md:text-xl sm:flex dark:bg-green-dark">
         <HeaderNavLinks />
       </div>
       <ThemeSwitch />
