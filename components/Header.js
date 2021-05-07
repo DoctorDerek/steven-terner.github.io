0f9e4458b1a1e5228b3f192d4bef2430215748b4
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-07 17:19:13 -0500

    Fix order to remove need for flex-row-reverse

--- components/Header.js
+++ components/Header.js
@@ -4,12 +4,12 @@ import HeaderNavLinks from "@/components/HeaderNavLinks"
 
 export default function Header() {
   return (
-    <div className="flex flex-row-reverse items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-row">
+    <div className="flex items-center justify-between w-full mt-8 mb-4 text-base leading-5">
       <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-100 xl:text-2xl md:text-xl sm:flex dark:bg-green-dark">
         <HeaderNavLinks />
       </div>
-      <ThemeSwitch />
       <MobileNav />
+      <ThemeSwitch />
     </div>
   )
 }
