367bc47359b4c0829cf7fc19ccefa235291dfb97
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-07 16:10:09 -0500

    WIP fix footer IEN spacing

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,8 @@ import siteMetadata from "@/data/siteMetadata"
 import SectionContainer from "@/components/SectionContainer"
 import HeaderNavLinks from "@/components/HeaderNavLinks"
 import Link from "@/components/Link"
+import Twemoji from "@/components/Twemoji"
+import Image from "@/components/Image"
 
 export default function Footer() {
   return (
@@ -18,11 +20,16 @@ export default function Footer() {
           key="IEN"
           href="https://IranianEconomicNews.com"
           className={
-            "xl:px-5 px-1 py-1 rounded-3xl dark:text-gray-100 hover:underline text-gray-900"
+            "xl:px-5 px-1 py-1 rounded-3xl dark:text-gray-100 hover:underline text-gray-900 flex space-x-1 flex-row-reverse sm:flex-row items-center w-full justify-between xl:justify-start xl:space-x-3"
           }
         >
-          Subscribe free to Iranian Economic News, an email newsletter by Steven
-          Terner
+          <Twemoji emoji="📰" size="w-8 h-8" />
+          <span>
+            Subscribe to Iranian Economic News, our free email newsletter
+          </span>
+          <div className="grid w-8 h-8">
+            <Image src="/static/images/IEN-logo.png" alt="IEN logo" />
+          </div>
         </Link>
       </footer>
     </SectionContainer>
