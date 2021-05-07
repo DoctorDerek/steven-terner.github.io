073eb2c539d533296dd67f06b0a922a1d91cb402
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-07 15:49:11 -0500

    Add styles to footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,8 +9,8 @@ export default function Footer() {
       <footer className="flex flex-col flex-wrap w-full py-1 mt-8 mb-0 font-serif text-2xl font-bold text-center rounded sm:text-xl sm:mb-8 sm:flex-row">
         <HeaderNavLinks location={"footer"} />
         <div className="flex justify-center flex-grow py-8 space-x-2 text-gray-900 sm:py-0 sm:justify-end dark:text-gray-100">
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>
+          <div className="py-1 text-gray-900 rounded-3xl dark:text-gray-100 hover:underline">{`© ${new Date().getFullYear()}`}</div>
+          <div className="py-1 text-gray-900 rounded-3xl dark:text-gray-100 hover:underline">
             <Link href="/">{siteMetadata.author}</Link>
           </div>
         </div>
