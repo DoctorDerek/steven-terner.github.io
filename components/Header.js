988b4648088ced8e750c0c5065591e9415c748c3
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-10 21:59:22 -0500

    Add antialising to outline of logo with Tailwind CSS filter
    Filters & Backdrop Filters – What's New in Tailwind CSS
    https://www.youtube.com/watch?v=qGEZdGMgXL4

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <SectionContainer header={true} className="pt-10">
       <Link href="/" aria-label="Steven Terner, LLC" className={null}>
-        <div className="text-gray-100 stroke-current stroke-0 dark:stroke-1 fill-green-dark">
+        <div className="text-gray-100 stroke-current stroke-0 fill-green-dark filter dark:blur-[0.5px] dark:stroke-2 transition-all duration-700 antialiased drop-shadow-md">
           <Logo />
         </div>
       </Link>
