c89824e284842ed6f2635866de856c4944283aad
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-11 18:24:34 -0500

    Change Link style to border

--- components/Link.js
+++ components/Link.js
@@ -1,12 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from "next/link"
 
-const CustomLink = ({ href, className, ...rest }) => {
+const classNames = (...classes) => classes.join(" ")
+
+// color can be "green" (default) or "black" text color
+// border can be "bottom" (default) or "none" border width
+// darkModeBorder can be "green" (default) or "white" border
+const CustomLink = ({
+  href,
+  className,
+  color = "green",
+  border = "bottom",
+  darkModeBorder = "green",
+  ...rest
+}) => {
   const isInternalLink = href && href.startsWith("/")
   const isAnchorLink = href && href.startsWith("#")
 
-  const defaultClass =
-    "underline text-green-dark hover:text-gray-900 hover:bg-green-light dark:text-gray-100 dark:hover:text-gray-900 dark:border-b-2 dark:border-solid dark:border-green-dark dark:no-underline"
+  const defaultClass = classNames(
+    border === "bottom"
+      ? "border-green-dark"
+      : "border-transparent dark:border-transparent",
+    color === "green" ? "text-green-dark" : "text-gray-900",
+    darkModeBorder === "green"
+      ? "hover:border-green-dark"
+      : "dark:border-gray-100 dark:hover:border-gray-100",
+    "no-underline hover:text-gray-900 hover:bg-green-light dark:text-gray-100 dark:hover:text-gray-900 border-solid dark:no-underline border-b-2 "
+  )
   if (className) {
     if (!className.match(/text-/)) {
       // no-underline works as a prop, but not text-gray-100, text-gray-900, etc.
