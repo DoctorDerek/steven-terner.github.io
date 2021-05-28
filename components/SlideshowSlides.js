0ba31034cbfafd00d3082ef8af39aa19ea9dcf85
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-28 00:13:28 -0500

    chore: convert SlideshowSlides from const to function

--- components/SlideshowSlides.js
+++ components/SlideshowSlides.js
@@ -1,61 +1,60 @@
-import React from "react"
 import Image from "@/components/Image"
 import SlideshowSlide from "@/components/SlideshowSlide"
 
-const SlideshowSlides = () => (
-  <>
-    <SlideshowSlide>
-      <Image
-        alt="ocean"
-        src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
-        sizes="portrait"
-      />
-      <Image
-        alt="ocean"
-        src="/static/images/behzad-ghaffarian-agGyKOiowuk-unsplash.jpg"
-        sizes="portrait"
-      />
-      <Image
-        alt="ocean"
-        src="/static/images/mehrshad-rajabi-UI8kIVUu7oI-unsplash.jpg"
-        sizes="portrait"
-      />
-    </SlideshowSlide>
-    <SlideshowSlide>
-      <Image
-        alt="ocean"
-        src="/static/images/milad-b-fakurian-q539g9f0cYw-unsplash.jpg"
-        sizes="landscape"
-      />
-      <Image
-        alt="ocean"
-        src="/static/images/mostafa-meraji-wH_5YAylHnc-unsplash.jpg"
-        sizes="landscape"
-      />
-      <Image
-        alt="ocean"
-        src="/static/images/pourya-daneshvar-cYmRHwySr3U-unsplash.jpg"
-        sizes="landscape"
-      />
-    </SlideshowSlide>
-    <SlideshowSlide>
-      <Image
-        alt="ocean"
-        src="/static/images/roozbeh-eslami-o2E9zgoRFgc-unsplash.jpg"
-        sizes="landscape"
-      />
-      <Image
-        alt="ocean"
-        src="/static/images/sajad-nori-cacpYWjYEt0-unsplash.jpg"
-        sizes="landscape"
-      />
-      <Image
-        alt="ocean"
-        src="/static/images/Imports, Exports, and International Partnerships photo.jpg"
-        sizes="landscape"
-      />
-    </SlideshowSlide>
-  </>
-)
-
-export default SlideshowSlides
+export default function SlideshowSlides() {
+  return (
+    <>
+      <SlideshowSlide>
+        <Image
+          alt="ocean"
+          src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
+          sizes="portrait"
+        />
+        <Image
+          alt="ocean"
+          src="/static/images/behzad-ghaffarian-agGyKOiowuk-unsplash.jpg"
+          sizes="portrait"
+        />
+        <Image
+          alt="ocean"
+          src="/static/images/mehrshad-rajabi-UI8kIVUu7oI-unsplash.jpg"
+          sizes="portrait"
+        />
+      </SlideshowSlide>
+      <SlideshowSlide>
+        <Image
+          alt="ocean"
+          src="/static/images/milad-b-fakurian-q539g9f0cYw-unsplash.jpg"
+          sizes="landscape"
+        />
+        <Image
+          alt="ocean"
+          src="/static/images/mostafa-meraji-wH_5YAylHnc-unsplash.jpg"
+          sizes="landscape"
+        />
+        <Image
+          alt="ocean"
+          src="/static/images/pourya-daneshvar-cYmRHwySr3U-unsplash.jpg"
+          sizes="landscape"
+        />
+      </SlideshowSlide>
+      <SlideshowSlide>
+        <Image
+          alt="ocean"
+          src="/static/images/roozbeh-eslami-o2E9zgoRFgc-unsplash.jpg"
+          sizes="landscape"
+        />
+        <Image
+          alt="ocean"
+          src="/static/images/sajad-nori-cacpYWjYEt0-unsplash.jpg"
+          sizes="landscape"
+        />
+        <Image
+          alt="ocean"
+          src="/static/images/Imports, Exports, and International Partnerships photo.jpg"
+          sizes="landscape"
+        />
+      </SlideshowSlide>
+    </>
+  )
+}
