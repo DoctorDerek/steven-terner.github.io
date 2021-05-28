af9107229a38a62679ae06e4ef0e0973bd87f226
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-28 00:09:22 -0500

    Revert "chore: convert from arrow function to function"
    This reverts commit 841e2b2120e01a8eda7495e695b78e9ef9af279c.

--- components/SlideshowSlides.js
+++ components/SlideshowSlides.js
@@ -2,69 +2,69 @@ import React from "react"
 import Image from "@/components/Image"
 import SlideshowSlide from "@/components/SlideshowSlide"
 
-export default function SlideshowSlides() {
-  return (
-    <>
-      <SlideshowSlide>
-        <Image
-          alt="ocean"
-          src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
-          sizes="portrait"
-          priority={true}
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/behzad-ghaffarian-agGyKOiowuk-unsplash.jpg"
-          sizes="portrait"
-          priority={true}
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/mehrshad-rajabi-UI8kIVUu7oI-unsplash.jpg"
-          sizes="portrait"
-          priority={true}
-        />
-      </SlideshowSlide>
-      <SlideshowSlide>
-        <Image
-          alt="ocean"
-          src="/static/images/milad-b-fakurian-q539g9f0cYw-unsplash.jpg"
-          sizes="landscape"
-          priority={true}
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/mostafa-meraji-wH_5YAylHnc-unsplash.jpg"
-          sizes="landscape"
-          priority={true}
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/pourya-daneshvar-cYmRHwySr3U-unsplash.jpg"
-          sizes="landscape"
-          priority={true}
-        />
-      </SlideshowSlide>
-      <SlideshowSlide>
-        <Image
-          alt="ocean"
-          src="/static/images/roozbeh-eslami-o2E9zgoRFgc-unsplash.jpg"
-          sizes="landscape"
-          priority={true}
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/sajad-nori-cacpYWjYEt0-unsplash.jpg"
-          sizes="landscape"
-          priority={true}
-        />
-        <Image
-          alt="ocean"
-          src="/static/images/Imports, Exports, and International Partnerships photo.jpg"
-          sizes="landscape"
-          priority={true}
-        />
-      </SlideshowSlide>
-    </>
-  )
-}
+const SlideshowSlides = () => (
+  <>
+    <SlideshowSlide>
+      <Image
+        alt="ocean"
+        src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
+        sizes="portrait"
+        priority={true}
+      />
+      <Image
+        alt="ocean"
+        src="/static/images/behzad-ghaffarian-agGyKOiowuk-unsplash.jpg"
+        sizes="portrait"
+        priority={true}
+      />
+      <Image
+        alt="ocean"
+        src="/static/images/mehrshad-rajabi-UI8kIVUu7oI-unsplash.jpg"
+        sizes="portrait"
+        priority={true}
+      />
+    </SlideshowSlide>
+    <SlideshowSlide>
+      <Image
+        alt="ocean"
+        src="/static/images/milad-b-fakurian-q539g9f0cYw-unsplash.jpg"
+        sizes="landscape"
+        priority={true}
+      />
+      <Image
+        alt="ocean"
+        src="/static/images/mostafa-meraji-wH_5YAylHnc-unsplash.jpg"
+        sizes="landscape"
+        priority={true}
+      />
+      <Image
+        alt="ocean"
+        src="/static/images/pourya-daneshvar-cYmRHwySr3U-unsplash.jpg"
+        sizes="landscape"
+        priority={true}
+      />
+    </SlideshowSlide>
+    <SlideshowSlide>
+      <Image
+        alt="ocean"
+        src="/static/images/roozbeh-eslami-o2E9zgoRFgc-unsplash.jpg"
+        sizes="landscape"
+        priority={true}
+      />
+      <Image
+        alt="ocean"
+        src="/static/images/sajad-nori-cacpYWjYEt0-unsplash.jpg"
+        sizes="landscape"
+        priority={true}
+      />
+      <Image
+        alt="ocean"
+        src="/static/images/Imports, Exports, and International Partnerships photo.jpg"
+        sizes="landscape"
+        priority={true}
+      />
+    </SlideshowSlide>
+  </>
+)
+
+export default SlideshowSlides
