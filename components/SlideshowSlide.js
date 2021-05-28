bc78efa581320367cbfbb88298b0787e210ff4af
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-28 00:14:46 -0500

    fix: try to fix Keen Slider CLS issues by "lazy loading" {children} of SlideshowSlide with useEffect

--- components/SlideshowSlide.js
+++ components/SlideshowSlide.js
@@ -1,8 +1,14 @@
-import React from "react"
-const SlideshowSlide = ({ children }) => (
-  <div className="grid grid-cols-3 space-x-0.5 sm:space-x-1 md:space-x-1.5 xl:space-x-2 transform-gpu keen-slider__slide">
-    {children}
-  </div>
-)
+import { useState, useEffect } from "react"
+
+function SlideshowSlide({ children }) {
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(() => setIsLoaded(() => true))
+
+  return (
+    <div className="grid grid-cols-3 space-x-0.5 sm:space-x-1 md:space-x-1.5 xl:space-x-2 transform-gpu keen-slider__slide">
+      {isLoaded && children}
+    </div>
+  )
+}
 
 export default SlideshowSlide
