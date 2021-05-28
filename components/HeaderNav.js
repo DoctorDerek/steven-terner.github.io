0f17f3af2f9e4feacec08b2b4e2258448c09a7c9
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-27 23:41:33 -0500

    fix: remove flex-wrap to fix mobile menu bug

--- components/HeaderNav.js
+++ components/HeaderNav.js
@@ -5,7 +5,7 @@ import { useState } from "react"
 export default function Header() {
   const [showNav, setShowNav] = useState(false)
   return (
-    <nav className="flex flex-wrap items-center justify-between w-full mt-8 mb-4 text-base leading-5 sm:flex-nowrap">
+    <nav className="flex items-center justify-between w-full mt-8 mb-4 text-base leading-5">
       <div className="items-center justify-around hidden w-full py-1 mr-2 font-serif font-bold text-center border-2 border-gray-900 border-solid rounded-3xl dark:border-gray-100 xl:text-2xl md:text-xl dark:bg-green-dark sm:flex">
         <HeaderNavLinks />
       </div>
