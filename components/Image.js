c1fadae591591843ff3ec0f417c57dbbc45fa924
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-28 00:09:27 -0500

    Revert "fix: preload all slideshow images to try to reduce CLS with keen slider"
    This reverts commit 98cd3ea4bdff069b7d32c0e1bfdf4c06c6978a54.

--- components/Image.js
+++ components/Image.js
@@ -12,7 +12,6 @@ const CustomImage = ({
   className = "object-cover", // Assume dynamic sizing
   sizes = "100vw", // Next.js default
   quality = "75", // Next.js default
-  priority = false, // preload images
   ...rest
 }) => {
   const validSizes = {
@@ -39,7 +38,6 @@ const CustomImage = ({
     className,
     sizes,
     quality,
-    priority,
     ...rest,
   }
 
--- components/SlideshowSlides.js
+++ components/SlideshowSlides.js
@@ -9,19 +9,16 @@ const SlideshowSlides = () => (
         alt="ocean"
         src="/static/images/ahmad-barshod-miCPhkJLYs8-unsplash.jpg"
         sizes="portrait"
-        priority={true}
       />
       <Image
         alt="ocean"
         src="/static/images/behzad-ghaffarian-agGyKOiowuk-unsplash.jpg"
         sizes="portrait"
-        priority={true}
       />
       <Image
         alt="ocean"
         src="/static/images/mehrshad-rajabi-UI8kIVUu7oI-unsplash.jpg"
         sizes="portrait"
-        priority={true}
       />
     </SlideshowSlide>
     <SlideshowSlide>
@@ -29,19 +26,16 @@ const SlideshowSlides = () => (
         alt="ocean"
         src="/static/images/milad-b-fakurian-q539g9f0cYw-unsplash.jpg"
         sizes="landscape"
-        priority={true}
       />
       <Image
         alt="ocean"
         src="/static/images/mostafa-meraji-wH_5YAylHnc-unsplash.jpg"
         sizes="landscape"
-        priority={true}
       />
       <Image
         alt="ocean"
         src="/static/images/pourya-daneshvar-cYmRHwySr3U-unsplash.jpg"
         sizes="landscape"
-        priority={true}
       />
     </SlideshowSlide>
     <SlideshowSlide>
@@ -49,19 +43,16 @@ const SlideshowSlides = () => (
         alt="ocean"
         src="/static/images/roozbeh-eslami-o2E9zgoRFgc-unsplash.jpg"
         sizes="landscape"
-        priority={true}
       />
       <Image
         alt="ocean"
         src="/static/images/sajad-nori-cacpYWjYEt0-unsplash.jpg"
         sizes="landscape"
-        priority={true}
       />
       <Image
         alt="ocean"
         src="/static/images/Imports, Exports, and International Partnerships photo.jpg"
         sizes="landscape"
-        priority={true}
       />
     </SlideshowSlide>
   </>
