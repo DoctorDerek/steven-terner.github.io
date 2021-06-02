0b3afdfcc71bae9721ee40359fb61415f918eff7
Dr. Derek Austin <derekraustin@gmail.com>
2021-06-02 18:24:54 -0500

    refactor: replace Image (next/image) with Image (@/components/Image)

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,17 @@
-import ImageFixed from "next/image"
+import Image from "@/components/Image"
 import Link from "@/components/Link"
 
 const Card = ({ alt, src, link, heading, children }) => {
   return (
     <div className="py-8 md:px-3 sm:px-4 sm:first:pl-0 sm:first:pr-4 sm:last:pl-4 sm:last:pr-0 md:first:pr-3 md:last:pl-3 sm:py-0 first:pt-0 first:pb-8 last:pt-8 last:pb-0 sm:first:py-0 sm:last:py-0">
       {src && (
-        <div className="relative m-auto h-36">
-          <ImageFixed
+        <div className="grid w-full h-36">
+          <Image
             alt={alt}
             src={src}
-            className="object-cover rounded-full"
-            layout="fill"
-            sizes="33vw"
+            sizes="portrait"
             preload={true}
+            className="rounded-full"
           />
         </div>
       )}
